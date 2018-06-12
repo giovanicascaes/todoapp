5dcd287868062175388b49f41129e3b38cfb8a42
Giovani Cascaes <giovanicascaes@gmail.com>
2018-06-11 23:40:18 -0300

    Adicionada linha no texto de todos concluídos

--- Todo.js
+++ Todo.js
@@ -7,17 +7,20 @@ import {StyleSheet, Text, View} from 'react-native';
 import Checkbox from "./Checkbox";
 import RemoveButton from "./RemoveButton";
 
-export default ({children, onRemove, onToggle}) =>
-    <View style={styles.todo}>
+export default ({children, onRemove, onToggle}) => {
+    const {done} = children;
+
+    return <View style={styles.todo}>
         <RemoveButton onClick={onRemove}
                       style={styles.remove} />
-        <Text style={styles.text}>
+        <Text style={done ? styles.strokeText : styles.text}>
             {children.text}
         </Text>
-        <Checkbox checked={children.done}
+        <Checkbox checked={done}
                   onClick={onToggle}
                   style={styles.check} />
     </View>;
+};
 
 const styles = StyleSheet.create({
     todo: {
@@ -32,6 +35,11 @@ const styles = StyleSheet.create({
     text: {
         flex: 1,
         color: '#3B3B3B'
+    },
+    strokeText: {
+        flex: 1,
+        color: '#3B3B3B',
+        textDecorationLine: 'line-through'
     },
     check: {
         alignSelf: 'flex-end'
